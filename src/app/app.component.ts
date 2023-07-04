198caf76b13671cf98035fbf74bb7fda0de969e1
Abdelrahaman Osama <aerosama793@gmail.com>
2023-07-04 15:42:46 +0300

    Now a field is shown according to what is dropped, still looking for better ways to implement it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from "@angular/cdk/drag-drop";
+import { CdkDragDrop, moveItemInArray, transferArrayItem, copyArrayItem } from "@angular/cdk/drag-drop";
+import { field } from './global.model';
 
 @Component({
   selector: 'app-root',
@@ -9,34 +10,174 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from "@angular/cdk/dr
 export class AppComponent {
   title = 'angular-drag-drop-test';
 
-  goodGames = [
-    "Mafia 2", "GTA V", "Tunic", "Batman Arkham Night", "Shadow Of The Tomb Raider", "God Of War", "Uncharted 4",
+  // goodGames = [
+  //   "Mafia 2",
+  //   "GTA V",
+  //   "Tunic",
+  //   "Batman Arkham Night",
+  //   "Shadow Of The Tomb Raider",
+  //   "God Of War",
+  //   "Uncharted 4",
+  // ];
+
+  // wishlistGames = [
+  //   "Elden Ring",
+  //   "Mafia 3",
+  //   "Hogwarts Legacy",
+  //   "The Witcher 3",
+  //   "Death's Door",
+  //   "Rise Of The Tomb Raider",
+  // ];
+
+  fieldModels: Array<field> = [
+    {
+      "type": "text",
+      "icon": "fa-font",
+      "label": "Text",
+      "description": "Enter your name",
+      "placeholder": "Enter your name",
+      "className": "form-control",
+      "subtype": "text",
+      "regex": "",
+      "handle": true
+    },
+    {
+      "type": "email",
+      "icon": "fa-envelope",
+      "required": true,
+      "label": "Email",
+      "description": "Enter your email",
+      "placeholder": "Enter your email",
+      "className": "form-control",
+      "subtype": "text",
+      "regex": "^([a-zA-Z0-9_.-]+)@([a-zA-Z0-9_.-]+)\.([a-zA-Z]{2,5})$",
+      "errorText": "Please enter a valid email",
+      "handle": true
+    },
+    {
+      "type": "phone",
+      "icon": "fa-phone",
+      "label": "Phone",
+      "description": "Enter your phone",
+      "placeholder": "Enter your phone",
+      "className": "form-control",
+      "subtype": "text",
+      "regex": "^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$",
+      "errorText": "Please enter a valid phone number",
+      "handle": true
+    },
+    {
+      "type": "date",
+      "icon": "fa-calendar",
+      "label": "Date",
+      "placeholder": "Date",
+      "className": "form-control"
+    },
+    {
+      "type": "datetime-local",
+      "icon": "fa-calendar",
+      "label": "DateTime",
+      "placeholder": "Date Time",
+      "className": "form-control"
+    },
+    {
+      "type": "textarea",
+      "icon": "fa-text-width",
+      "label": "Textarea"
+    },
+    {
+      "type": "checkbox",
+      "required": true,
+      "label": "Checkbox",
+      "icon": "fa-list",
+      "description": "Checkbox",
+      "inline": true,
+      "values": [
+        {
+          "label": "Option 1",
+          "value": "option-1"
+        },
+        {
+          "label": "Option 2",
+          "value": "option-2"
+        }
+      ]
+    },
+    {
+      "type": "radio",
+      "icon": "fa-list-ul",
+      "label": "Radio",
+      "description": "Radio boxes",
+      "values": [
+        {
+          "label": "Option 1",
+          "value": "option-1"
+        },
+        {
+          "label": "Option 2",
+          "value": "option-2"
+        }
+      ]
+    },
+    {
+      "type": "button",
+      "icon": "fa-paper-plane",
+      "subtype": "submit",
+      "label": "Submit"
+    }
   ];
 
-  wishlistGames = [
-    "Elden Ring",
-    "Mafia 3",
-    "Hogwarts Legacy",
-    "The Witcher 3",
-    "Death's Door",
-    "Rise Of The Tomb Raider",
+  bodyFields: Array<field> = [
   ];
+
+  body: any = {
+    name: 'Form/Document Name',
+    description: 'Form/Document Description',
+    theme: {
+      bgColor: "ffffff",
+      textColor: "555555",
+      bannerImage: ""
+    },
+    attributes: this.bodyFields
+  };
+
 
   onDrop(event: CdkDragDrop<string[]>) {
-    if (event.previousContainer === event.container) {
-      moveItemInArray(
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      )
-    } else {
-      transferArrayItem(
+    if (event.previousContainer !== event.container) {
+      copyArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
         event.currentIndex,
       )
+    } else {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      )
     }
-
   }
+
+  toggleValue(item: any){}
+  
+  submit(){}
+
+  // onDrop(event: CdkDragDrop<string[]>) {
+  //   if (event.previousContainer === event.container) {
+  //     moveItemInArray(
+  //       event.container.data,
+  //       event.previousIndex,
+  //       event.currentIndex,
+  //     )
+  //   } else {
+  //     transferArrayItem(
+  //       event.previousContainer.data,
+  //       event.container.data,
+  //       event.previousIndex,
+  //       event.currentIndex,
+  //     )
+  //   }
+
+  // }
 }
